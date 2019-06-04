2645440416e731555f2afb4d9389353bef082985
atjhoendz <achunarmando@gmail.com>
2019-06-04 16:30:07 +0700

    renewal resourse

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,50 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
-    new fullpage('#fullpage', {
-        navigation: true,
+    var myFullpage = new fullpage('#fullpage', {
+        // navigation: true,
         responsive:700,
-        navigationPosition:'right',
+        menu: '#menu',
+        // scrollOverflow:true,
+        // navigationPosition:'right',
         // anchors: ['home', 'about', 'talkshow', 'competition', 'semnas', 'timeline', 'contact'],
         // parallax: true,
         // autoScroll:true,
+        onLeave: function(origin, destination, direction){
+            if(origin.anchor == 'home' && direction == 'down'){
+                $('#navbar').removeClass('transparent');
+                $('#navbar').removeClass('z-depth-0');
+                $('#navbar').addClass('z-depth-1');
+                $('#navbar').addClass('bg_navbar');
+            }else if(origin.anchor == 'about' && direction == 'up'){
+                $('#navbar').removeClass('z-depth-1');
+                $('#navbar').addClass('z-depth-0');
+                $('#navbar').removeClass('bg_navbar');
+                $('#navbar').addClass('transparent');
+            }else if(destination.anchor == 'sponsormedpart' && origin.anchor == 'contact'){
+                $('#sponsormedpart-section').addClass('addPadding_top');
+            }else if(destination.anchor == 'about' && origin.anchor == 'talkshow'){
+                $('#about-section').addClass('addPadding_top');
+            }else if(destination.anchor == 'talkshow' && origin.anchor == 'competition'){
+                $('#talkshow-section').addClass('addPadding_top');
+            }
+            else{
+                $('#sponsormedpart-section').removeClass('addPadding_top');
+                $('about-section').removeClass('addPadding_top');
+                $('#talkshow-section').removeClass('addPadding_top');
+            }
+        },
+        afterLoad: function(origin, destination, direction){
+            if(destination.anchor == 'home'){
+                $('#navbar').removeClass('z-depth-1');
+                $('#navbar').addClass('z-depth-0');
+                $('#navbar').removeClass('bg_navbar');
+                $('#navbar').addClass('transparent');
+            }else if(destination.anchor == 'sponsormedpart' && origin.anchor == 'contact'){
+                $('#sponsormedpart-section').addClass('addPadding_top');
+            }else{
+                $('#sponsormedpart-section').removeClass('addPadding_top');
+            }
+        }
     });
     window.sr = ScrollReveal({reset:true});
     sr.reveal('.animleft', {
@@ -47,5 +85,23 @@ $(document).ready(function(){
     
     $('#btnLogin').on('click', function(){
         location.replace(baseUrl + '/login');
-    })
+    });
+    // Change color navbar onscroll
+    // var scroll_start = 0;
+    // var startchange = $('#about-section');
+    // var offset = startchange.offset();
+    // if(startchange.length){
+    //     alert('a');
+    //     $(window).scroll(function(){
+    //         alert('scroll');
+    //         scroll_start = $(this).scrollTop();
+    //         if(scroll_start > offset.top){
+    //             $('#navbar').removeClass('transparent');
+    //             $('#navbar').addClass('blue');
+    //         }else{
+    //             $('#navbar').removeClass('blue');
+    //             $('#navbar').addClass('transparent');
+    //         }
+    //     });
+    // }
 });
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,37 +6,37 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         {!! MaterializeCSS::include_full() !!}
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        <script src="https://unpkg.com/scrollreveal"></script>
+        {{-- <script src="https://unpkg.com/scrollreveal"></script> --}}
         
         <title>{{ config('app.name') }}</title>
     </head>
     <body id="b_ifest19">
         <div class="navbar-fixed">
-            <nav class="transparent z-depth-0">
+            <nav id="navbar" class="transparent z-depth-0">
                 <div class="nav-wrapper">
                     <a data-menuanchor="home" href="#home" onclick="$('#header-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});" class="brand-logo"><img src="{{url('/img/logo_nav.png')}}" class="logo_nav" alt="IFest"></a>
                     <a href="#mobile-demo" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                    <ul class="right hide-on-med-and-down">
+                    <ul id="menu" class="right hide-on-med-and-down">
                         <li data-menuanchor='about'><a href="#about" onclick="$('#about-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">About</a></li>
                         <li data-menuanchor='talkshow'><a href="#talkshow" onclick="$('#talkshow-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Talkshow</a></li>
                         <li data-menuanchor='competition'><a href="#competition" onclick="$('#competition-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Competition</a></li>
                         <li data-menuanchor='semnas'><a href="#semnas" onclick="$('#semnas-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Seminar & IT Expo</a></li>
                         <li data-menuanchor='timeline'><a href="#timeline" onclick="$('#timeline-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Timeline</a></li>
                         <li data-menuanchor='contact'><a href="#contact" onclick="$('#contact-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Contact</a></li>
-                        <li><button id="btnLogin" class="waves-effect waves-light btn-small blue btnMain">Login/SignUp</button></li>
+                        <li><button id="btnLogin" class="waves-effect waves-light btn-small blue btnMain">Login</button></li>
                     </ul>
                 </div>
             </nav>
         </div>
 
         <ul class="sidenav" id="mobile-demo">
-                <li><a href="#about-section" onclick="$('#about-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">About</a></li>
-                <li><a href="#talkshow-section" onclick="$('#talkshow-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Talkshow</a></li>
-                <li><a href="#competition-section" onclick="$('#competition-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Competition</a></li>
-                <li><a href="#semnas-section" onclick="$('#semnas-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Seminar & IT Expo</a></li>
-                <li><a href="#timeline-section" onclick="$('#timeline-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Timeline</a></li>
-                <li><a href="#contact-section" onclick="$('#contact-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Contact</a></li>
-                <li><button id="btnLogin" class="waves-effect waves-light btn-small blue btnMain">Login/Daftar</button></li>
+            <li><a href="#about-section" onclick="$('#about-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">About</a></li>
+            <li><a href="#talkshow-section" onclick="$('#talkshow-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Talkshow</a></li>
+            <li><a href="#competition-section" onclick="$('#competition-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Competition</a></li>
+            <li><a href="#semnas-section" onclick="$('#semnas-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Seminar & IT Expo</a></li>
+            <li><a href="#timeline-section" onclick="$('#timeline-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Timeline</a></li>
+            <li><a href="#contact-section" onclick="$('#contact-section').animatescroll({scrollSpeed:1500,easing:'easeInOutBack'});">Contact</a></li>
+            <li><button id="btnLogin" class="waves-effect waves-light btn-small blue btnMain">Login</button></li>
         </ul>
 
         <div id="fullpage">
@@ -48,8 +48,8 @@
                             {{-- <img src="{{url('/img/himatif.png')}}" class="imglogo" alt="logoHimatif"> --}}
                             <h2 class="text_shadow" style="margin-top:0">INFORMATICS FESTIVAL 2019</h2>
                             <h3 class="text_shadow" style="font-family:'MADE Evolve Sans EVO';">INNOVATE TO ELEVATE</h3>
-                            <a href="#about"><button class="waves-effect waves-light btn-small light-blue btnMain_border">Tentang</button></a>
-                            <button class="waves-effect waves-light btn-small light-blue btnMain">Login/SignUp</button>
+                            <a href="#about"><button class="waves-effect waves-light btn-small light-blue btnMain_border">About</button></a>
+                            <button class="waves-effect waves-light btn-small light-blue btnMain">Login</button>
                         </div>
                     </div>
                 </div>
@@ -63,8 +63,11 @@
                             {{-- <img src="{{url('/img/himatif.png')}}" class="logoBE" alt="logoHimatif"> --}}
                         </div>
                         <div class="col m6">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem expedita maiores provident. Reprehenderit ipsam, nisi ab perferendis illum nobis debitis saepe, praesentium quaerat recusandae laboriosam minus rem earum! Veniam, maxime!
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam explicabo tenetur sed numquam aliquid mollitia? Laborum itaque quos harum vero repellendus officiis vel quidem velit! Iure unde corporis tempore nobis.
+                            <p>
+                                    <br>&nbsp;&nbsp;&nbsp;&nbsp;IFest atau Informatics Festival merupakan rangkaian acara IT tahunan yang diselenggarakan oleh Himpunan Mahasiswa Teknik Informatika Universitas Padjadjaran sejak tahun 2015.
+                                    <br>&nbsp;&nbsp;&nbsp;&nbsp;Tahun ini IFest membawa tema <span style="font-style:italic; color:yellow;">"Boot Up the Nations with Tech Innovations to Conquer Industrial Revolution"</span>, dengan rangkain acara Talkshow, IFest Competition, Seminar Nasional, IT Expo dan ditutup dengan hiburan sebagai closing ceremonial.
+                                    <br>&nbsp;&nbsp;&nbsp;&nbsp;Dengan diadakannya IFest ini diharapkan dapat mengembangkan wawasan pengetahuan masyarkat Indonesia di bidang teknologi serta memfasilitasi bibit unggul bangsa untuk saling berkompetisi menjadi yang terbaik.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -133,15 +136,41 @@
                 </div>
             </div>
 
-            <div id="contact-section" class="section" data-anchor="contact">
+            <div id="sponsormedpart-section" class="section" data-anchor="sponsormedpart">
                 <div class="container">
                     <div class="row">
-                        <h1>Contact</h1>
+                        <h1>Our Sponsor</h1>
                     </div>
+                </div>
+            </div>
+
+            <div id="contact-section" class="section fp-auto-height-responsive" data-anchor="contact">
+                <div class="container">
+                    <img src="{{asset('img/logo_nav.png')}}" alt="logo_IFest" class="logoFooter">
+                    <div class="right">
+                        <img src="{{asset('img/himatif.png')}}" alt="logo_himatif" class="logoFooter">
+                        <img src="{{asset('img/unpad.png')}}" alt="logo_unpad" class="logoFooter">
+                    </div>
+                    <div class="row">
+                        <div class="col s6">
+                            <p>
+                                ig: ifestunpad <br>
+                                line: @ifestunpad <br>
+                                email: informaticsfest@gmail.com
+                            </p>
+                        </div>
+                        <div class="col s6">
+                            (Alamat)Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt velit quod aperiam repellendus voluptates quisquam omnis ipsum vitae placeat facilis consectetur quidem excepturi, sint, at ipsam possimus sed quibusdam. Perferendis.
+                        </div>
+                    </div>
+                </div>
+                <div class="footer">
+                    <span>&copy IFest 2019 - HIMATIF FMIPA UNPAD</span>
                 </div>
             </div>
         </div>
 
+        {{-- <script src="{{asset('js/scrolloverflow.js')}}"></script> --}}
         <script src="{{asset('js/fullpage.min.js')}}"></script>
         <script src="{{asset('js/home.js')}}"></script>
         {{-- <script src="{{asset('js/animatescroll.min.js')}}"></script> --}}
